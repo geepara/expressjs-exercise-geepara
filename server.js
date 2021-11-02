0e62001905f1c3ac216b84bcda52cf1cf53db5a5
geepara <gpara1697@gmail.com>
2021-11-02 13:11:36 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 7000;
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/", (request, response) => {
+  response.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Express app is running on port ${PORT}`);
+});
